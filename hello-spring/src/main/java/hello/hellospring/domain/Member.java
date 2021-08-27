7de31ffe2b52d06853eab682fb9d44819f7b848b
rissins <tnalsqqq@naver.com>
2021-08-27 23:03:56 +0900

    JPA, 스프링데이터 JPA 공부

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY : db가 자동으로 알아서  생성해주는것
     private Long id;
     private String name;
 
